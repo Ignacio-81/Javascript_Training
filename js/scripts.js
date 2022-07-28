8989b0c58ff3464570418385259ab333e7ea38ad
Ignacio.Badella <77839981+Ignacio-81@users.noreply.github.com>
2022-07-28 17:09:29 -0300

    agregamos cards auto e imagenes

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,31 @@
 
 //const edadPersona = parseInt(prompt('Ingrese su edad'))
 //let dineroEncuenta = parseInt(prompt('Ingrese Dinero en su Cuenta'))
-let nombre = prompt('Ingrese Su nombre').toUpperCase();
+let userName = prompt('Please put your User Name').toUpperCase();
 let cuentaCarrito = 0 // lleva la cuenta de dinero en el carrito para la compra
 let ivaCgral = 0 //tiene el valor final + IVA de la cuenta de carrito
 let carrito = [] // lista de productos en el carrito
-let stockProductos = [] //Stock de productos de la tienda
+let stockProducts = [] //Stock de productos de la tienda
 let flagBr = false
 let promo = "" //Mensaje para las promos
 
 const iva = x => x * 0.21; //Funcion flecha para calcular el iva a la cuenta 
 
-/*Definimos la clase para los productos */
-class Producto {
-    constructor(nombre, precio, cantidad) {
-        this.nombre = nombre.toLowerCase();
-        this.precio = parseFloat(precio);
-        this.cantidad = parseInt(cantidad);
-        this.disponible = true;
+/*Product Class */
+class Product {
+    constructor(id, name, price, quantity, img) {
+        this.id = parseInt(id)
+        this.name = name.toLowerCase();
+        this.price = parseFloat(price);
+        this.quantity = parseInt(quantity);
+        this.img = img;
+        this.available = true;
     }
 
-    venta() {
-        this.cantidad--;
-        if (this.cantidad == 0) {
-            this.disponible = false;
+    sell() {
+        this.quantity--;
+        if (this.quantity == 0) {
+            this.available = false;
         }
     }
 
@@ -68,33 +70,54 @@ function promociones(edad, cuenta) {
     }
     return cuenta
 }
-/*Funcion para dar de alta el portfolio de productos*/
-function altaProductos() {
-    const vaso = new Producto("vaso", 50, 5)
-    const plato = new Producto("plato", 150, 10)
-    const heladera = new Producto("heladera", 45000, 10)
-    const caja = new Producto("caja", 25, 10)
-    const jarra = new Producto("jarra", 250, 10)
-    return [vaso, plato, heladera, caja, jarra]
+/*function to create new products*/
+function newProducts() {
+    const prod1 = new Product(1,"Ferrari Roja", 50, 5, "./assets/img/ferrari.jpg")
+    const prod2 = new Product(2,"Camaro Amarillo", 150, 10, "./assets/img/camaro.jpg")
+    const prod3 = new Product(3,"Combo x5", 45000, 10, "./assets/img/combo.jpg")
+    const prod4 = new Product(4,"Delorian Plata", 25, 10, "./assets/img/delorian.jpg")
+    const prod5 = new Product(5,"Audi TT Blanco", 250, 10, "./assets/img/audiTT.jpg")
+    return [prod1, prod2, prod3, prod4, prod5]
 }
 
-stockProductos = altaProductos()
-//Agregar nombre del usuario en el navegador
+stockProducts = newProducts()
+//Add user name to the navBar
 const titulo = document.querySelector("h1");
-titulo.textContent = "Hola "+nombre;
+titulo.textContent = "Hello "+userName;
 
 //Agregamos el titulo de los articulos disponibles.
 const tituloArt = document.querySelectorAll("h5");
 const tarjeta = document.body.querySelectorAll(".card-body"); 
-let i=0
-for (const producto of stockProductos){
-    tituloArt[i+2].innerText = producto.nombre;
-    let p = document.createElement("p");
-    p.className = "text-center"
-    p.innerHTML = `$${producto.precio}`
-    tarjeta[i+2].append(p);
-    i++;
-}
+
+stockProducts.forEach((product) => {
+    const idButton = `add-cart${product.id}` 
+    document.getElementById("section-card").innerHTML += `<div class="col mb-5">
+    <div class="card h-100">
+        <!-- Product image-->
+        <img class="card-img-top" src="${product.img}" alt="..." />
+        <!-- Product details-->
+        <div class="card-body p-4">
+            <div class="text-center">
+                <!-- Product name-->
+                <h5 class="fw-bolder">${product.name}</h5>
+                <!-- Product price-->
+                $${product.price}
+            </div>
+        </div>
+        <!-- Product actions-->
+        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center"><a id="${idButton}" class="btn btn-outline-dark mt-auto" href="#">Add to cart </a></div>
+        </div>
+    </div>
+</div>`;
+})
+
+stockProducts.forEach((product) => {
+    const idButton = `add-cart${product.id}` 
+    document.getElementById(idButton).addEventListener('click', () => {
+        alert("You added "+product.name.toUpperCase()+" to the Shopcart")
+    })
+})
 
 //DEJO ESTE CODIGO COMENTADO QUE ES DEL CODIGO ANTERIOR PARA RE UTILIZAR MAS ADELANTE
 /*
